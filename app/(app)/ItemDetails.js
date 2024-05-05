4ff4c790fcddf96384f8e6bc43679dd782a98797
MohammedRagab <mr1312064@gmail.com>
2024-05-05 14:07:55 +0300

    item Details done except for the header

--- app/(app)/ItemDetails.js
+++ app/(app)/ItemDetails.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Pressable,
+} from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function ItemDetails() {
   const { item } = useLocalSearchParams();
@@ -10,8 +22,16 @@ export default function ItemDetails() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [author, setAuthor] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [publisher, setPublisher] = useState("");
+  const [genre, setGenre] = useState("");
+  const [rate, setRate] = useState(4.56);
+
+  const handleAddToChart = (item) => {
+    console.log("added");
+  };
 
   useEffect(() => {
     if (!item) return; // Exit early if item is undefined
@@ -25,6 +45,9 @@ export default function ItemDetails() {
           setName(data.name);
           setPrice(data.price);
           setAuthor(data.author);
+          setImageUrl(data.imageUrl);
+          setPublisher(data.publisher);
+          setGenre(data.genre);
         } else {
           setError("No data found for this item.");
         }
@@ -55,10 +78,68 @@ export default function ItemDetails() {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Name: {name}</Text>
-      <Text>Price: {price}</Text>
-      <Text>Author: {author}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ height: "63%", borderColor: "#874f1f", borderWidth: 8 }}>
+        <Image style={styles.image} source={{ uri: imageUrl }} />
+      </View>
+      <View>
+        <Text style={[styles.text, { fontSize: 27 }]}>Book's Name: {name}</Text>
+        <Text style={[styles.text, { fontWeight: "bold" }]}>
+          Written by {author}
+        </Text>
+        <Text style={[styles.text, { fontWeight: "bold" }]}>
+          Rate: {rate} out of 5 stars
+        </Text>
+        <Text style={[styles.text, { fontWeight: "bold" }]}>
+          Published by {publisher}
+        </Text>
+        <Text style={[styles.text, { fontWeight: "bold" }]}>
+          Genre: {genre}
+        </Text>
+        <Text style={[styles.text, { fontWeight: "bold" }]}>
+          Price: {price}
+          <Text style={{ fontSize: 13, fontWeight: "500" }}>EGP</Text>
+        </Text>
+      </View>
+      <View style={styles.button}>
+        <Pressable
+          onPress={() => handleAddToChart(item.id)}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? "#874f1f" : "#ca6128",
+              padding: hp("1%"),
+              marginVertical: hp("1%"),
+              marginHorizontal: wp("1%"),
+              borderRadius: wp("2%"),
+              alignItems: "center",
+            },
+          ]}
+        >
+          <Text style={{ fontSize: 20, color: "#fff" }}>+ Add To Cart</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 6,
+    // backgroundColor: "#f5b596",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    // backgroundColor: "#",
+  },
+  text: {
+    paddingHorizontal: 7,
+    paddingVertical: 4,
+    fontSize: 17,
+    fontWeight: "500",
+    // color: "#fff",
+  },
+});
--- app/(app)/_layout.js
+++ app/(app)/_layout.js
@@ -1,17 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
-import HomeHeader from '../../components/HomeHeader'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import HomeHeader from "../../components/HomeHeader";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 export default function _layout() {
   return (
-    <Stack >
-      <Stack.Screen 
+    <Stack>
+      <Stack.Screen
         name="home"
         options={{
-          header:()=> <HomeHeader />
+          header: () => <HomeHeader />,
         }}
       />
+      {/* <Stack.Screen
+        name="ItemDetails"
+        options={{
+          headerShown: false,
+        }}
+      /> */}
     </Stack>
-  )
+  );
 }
